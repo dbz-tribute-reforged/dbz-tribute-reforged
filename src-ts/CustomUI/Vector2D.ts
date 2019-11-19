e97cef6e0e14c978571385150901d4ed75b311ce
randomkilla <randomkilla0@gmail.com>
2019-11-19 18:51:20 +1100

    Removed old Vector2D from CustomUI and moved it into Common

--- src-ts/CustomUI/Vector2D.ts
+++
@@ -1,9 +0,0 @@
-export class Vector2D {
-  constructor(public x: number = 0, public y:number = 0) {}
-
-  public add(v: Vector2D): this {
-    this.x += v.x;
-    this.y += v.y;
-    return this;
-  }
-}
