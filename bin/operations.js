dbe993a9421016536dbe1b78d0f6f7cb3dc5d676
ttay24 <tanner.taylor24@gmail.com>
2019-11-27 14:04:40 -0600

    build w3x into single file

--- bin/operations.js
+++ bin/operations.js
@@ -66,6 +66,11 @@ switch (operation) {
     break;
 
   case "package": {
-    console.log('TODO: package mpq');
+    exec("./bin/mpqtool", ["new", `${cwd}\\target\\${config.mapFolder}`, `build/${config.mapFolder}`], { maxBuffer: 1024/*bytes*/ * 2048 /*KB = 2MB total*/ }, function(err, data) {
+      if (err != null) {
+        console.log(err);
+        console.log("There was an error launching mpq tool.");
+      }
+    });
   }
 }
