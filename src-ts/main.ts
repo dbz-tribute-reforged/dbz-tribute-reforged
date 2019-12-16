b5b87fb4cfb52c276562eb5e7cacf8d6ff9cbe42
randomkilla <randomkilla0@gmail.com>
2019-12-16 18:38:32 +1100

    temporarily disabled ui to test sync issues, added in cell hs for 2nd form, moved some triggers to occur later (hopefully resolve sync problems)

--- src-ts/main.ts
+++ src-ts/main.ts
@@ -21,7 +21,7 @@ function tsMain() {
   Logger.doLogVerbose = false;
   Logger.doLogDebug = true;
   HostDetectSystem.onInit();
-  TimerStart(CreateTimer(), 1.0, false, () => {
+  TimerStart(CreateTimer(), 5.0, false, () => {
     DisplayTextToPlayer(GetLocalPlayer(), 0.0, 0.0, "Host detected=" + GetPlayerName(HostDetectSystem.GetHost()))
   })
 
@@ -34,7 +34,7 @@ function tsMain() {
     PathingCheck.Init();
     sagaManager = SagaManager.getInstance();
     
-    CustomUiTest();
+    // CustomUiTest();
     CustomPlayerTest();
     DestroyTimer(GetExpiredTimer());
   });
