4ebe784f2724b0544288a448e66c7d2c0df48a80
randomkilla <randomkilla0@gmail.com>
2020-01-17 02:53:19 +1100

    increased barrier wall from 7s to 10s duration

--- src-ts/CustomAbility/AbilityData/AbilitiesList.ts
+++ src-ts/CustomAbility/AbilityData/AbilitiesList.ts
@@ -1212,7 +1212,7 @@ export const AbilitiesList = [
     maxCd: 1,
     costType: "MP",
     costAmount: 0,
-    duration: 231,
+    duration: 330,
     updateRate: 0.03,
     castTime: 0.0,
     canMultiCast: true,
