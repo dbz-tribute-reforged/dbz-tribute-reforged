a7d0b82a3765944bcb19d37750b973adc6e0fd93
numberphone <alickip@gmail.com>
2021-03-16 15:38:45 -0500

    added ToP arena set ToP spawn locations to new values added tournament arena set tournament spawn locations added lookout tiles model moved some lookout stuff around

--- src-ts/Core/TournamentSystem/TournamentData.ts
+++ src-ts/Core/TournamentSystem/TournamentData.ts
@@ -2,8 +2,8 @@ import { Vector2D } from "Common/Vector2D";
 
 // maybe put all the tournament data into a config file
 export module TournamentData {
-  export const tournamentSpawn1: Vector2D = new Vector2D(22000, 23000);
-  export const tournamentSpawn2: Vector2D = new Vector2D(17200, 18300);
+  export const tournamentSpawn1: Vector2D = new Vector2D(17814, 19000);
+  export const tournamentSpawn2: Vector2D = new Vector2D(21511, 22612);
   export const tournamentWaitRoom1: Vector2D = new Vector2D(23300, 23000);
   export const tournamentWaitRoom2: Vector2D = new Vector2D(23300, 18500);
   export const tournamentBottomLeft: Vector2D = new Vector2D(16666, 17750);
@@ -16,8 +16,8 @@ export module TournamentData {
   );
   export const budokaiArenaBottomLeft: Vector2D = new Vector2D(12930, 19964);
   export const budokaiArenaTopRight: Vector2D = new Vector2D(16737, 23848);
-  export const budokaiSpawn1: Vector2D = new Vector2D(13352, 20467);
-  export const budokaiSpawn2: Vector2D = new Vector2D(16177, 23197);
+  export const budokaiSpawn1: Vector2D = new Vector2D(13778, 20747);
+  export const budokaiSpawn2: Vector2D = new Vector2D(15775, 22690);
   export const finalBattleDetector: Vector2D = new Vector2D(-555, 22860);
   export const finalBattleName: string = "Final Battle";
   export const finalBattleTime: number = 36 * 60;
